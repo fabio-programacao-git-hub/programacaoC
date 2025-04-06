0f2ae012e453b13a8326221a591aa53668e6d158
fabio-programacao-git-hub <fabiosz17@hotmail.com>
2025-04-06 20:51:50 +0000

    Super_trunfo_novato_intermediario

--- super_trunfo_intermediario.c
+++ super_trunfo_intermediario.c
@@ -8,13 +8,13 @@ int main(){
     char cidade_1[20] = "Rio Branco", cidade_2[20] = "Salvador";
     int populacao_1 = 365000, populacao_2 = 2417678;
     float area_1 = 8.835, area_2 = 693.442;
-    float pib_1 = 10955.000, pib_2 = 62954.000;
+    float pib_1 = 10955000.000, pib_2 = 62954000.000;
     int turismo_1 = 16, turismo_2 = 78;
 
-    float media_1 = populacao_1 / area_1;
-    float media_1_1 = pib_1 / populacao_1;
-    float media_2 = populacao_2 / area_2;
-    float media_2_2 = pib_2 / populacao_2;
+    float media_1 = (float) populacao_1 / area_1;
+    float media_1_1 = (float) pib_1 / populacao_1;
+    float media_2 = (float) populacao_2 / area_2;
+    float media_2_2 = (float) pib_2 / populacao_2;
 
     printf("\n");
     printf("Carta 1:\n");
@@ -25,7 +25,7 @@ int main(){
     printf("Área: %.3f km²\n", area_1);
     printf("PIB: %.3f bilhões de reais\n", pib_1);
     printf("Número de Pontos Turísticos: %d\n", turismo_1);
-    printf("Densidade Populacional: %.3f hab/km²\n", media_1);
+    printf("Densidade Populacional: %.f hab/km²\n", media_1);
     printf("PIB per Capita: %.3f mil reais\n", media_1_1);
     printf("\n");
 
@@ -37,7 +37,7 @@ int main(){
     printf("Área: %.3f km²\n", area_2);
     printf("PIB: %.3f bilhões de reais\n", pib_2);
     printf("Número de Pontos Turísticos: %d\n", turismo_2);
-    printf("Densidade Populacional: %.3f hab/km²\n", media_2);
+    printf("Densidade Populacional: %.f hab/km²\n", media_2);
     printf("PIB per Capita: %.3f mil reais\n", media_2_2);
     printf("\n");
 
